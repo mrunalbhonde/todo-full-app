b84440e85c9ea2eaf65adc4248b5ebd620bc753c
mrunalbhonde <bhondemrunal0@gmail.com>
2024-08-22 12:59:34 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,21 @@
 import { createTodo } from './types';
 import { updateTodo } from './types';
+import { todo } from './db';
 
 const express = require('express');
 const z = require("zod");
 const app = express();
 app.use(express.json());
 
-app.get('/todos', (req,res)=>{
-    res.send("hello, world!");
+app.get('/todos', async (req,res)=>{
+    const todos = await todo.find();
+    //console.log(todos); // will be a promise
+    res.json({
+        todos
+    })
 })
 
-app.post("/todos", function(req,res){
+app.post("/todos", async function(req,res){
     const createPayload = req.body;
     const parsePayload = createTodo.safeParse(createPayload);
     if(!parsePayload.success){
@@ -20,9 +25,18 @@ app.post("/todos", function(req,res){
         return;
     }
     // put it in the table if success
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo is created"
+    })
 })
 
-app.put("/completed", function(req, res){
+app.put("/completed", async function(req, res){
     const updatePayload = req.body;
     const parsePayload = updateTodo.safeParse(updatePayload);
     if(!parsePayload.success){
@@ -31,6 +45,16 @@ app.put("/completed", function(req, res){
         })
         return;
     }
+
+    await todo.update({
+        _id: updatePayload.id
+    }, {
+        completed: true
+    })
+
+    res.json({
+        msg: "Todo marked as completed"
+    })
 
 })
 
