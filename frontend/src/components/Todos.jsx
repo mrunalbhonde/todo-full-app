faf85f667ab105814c242ade3dece1dc2f8a1573
mrunalbhonde <bhondemrunal0@gmail.com>
2024-08-23 21:27:42 +0530

    basic structure

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,8 +1,24 @@
+//import { todo } from "../../../backend/db"
 
-export function Todos(){
+/* todos = [ 
+{ title: ,
+ desc: }
+
+]
+*/
+export function Todos({todos}){
+    // return <div>
+    //     <h1>Go to Gym</h1>
+    //     <h2>You need to have a core workout</h2>
+    //     <button>Mark as completed</button>
+    // </div>
     return <div>
-        <h1>Go to Gym</h1>
-        <h2>You need to have a core workout</h2>
-        <button>Mark as completed</button>
+        {todos.map(function(todo){
+            return <div>
+            <h1>{todo.title}</h1>
+            <h2>{todo.description}</h2>
+            <button>{todo.completed == true ? "Completed" : "Mark As Completed"}</button>
+            </div>
+        })}
     </div>
 }
